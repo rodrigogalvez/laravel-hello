56c622a7281e1285872e541574f51d57792593ae
Rodrigo Galvez <rodrigo.galvez.rojas@gmail.com>
2024-05-18 21:36:15 -0400

    Primer commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel ejemplo</title>
+</head>
+<body>
+    <nav>Menu aplicación</nav>
+    <head>Cabecera</head>
+    @yield('content')
+    <footer>Información de contacto</footer>
+</body>
+</html>
